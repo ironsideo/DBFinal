aef7b107a2d70c92c26d1f36e5ed0e040eee5315
io37@njit.edu <Wendy#@1>
2019-04-01 17:04:45 -0400

    first time commit: auth set up heroku

--- tests/Unit/isNumericTest.php
+++ tests/Unit/isNumericTest.php
@@ -13,6 +13,6 @@ class isNumericTest extends TestCase
     public function testFailure()
     {
         $user = User::inRandomOrder()->first();
-        $this->assertIsNumeric($user->id);
+        $this->isNull($user->id);
     }
 }

3bda819f171b16bd03ec32800946ffceb084c5ab
io37@njit.edu <Wendy#@1>
2019-04-01 17:47:34 -0400

    first time commit: auth set up heroku

--- tests/Unit/DataTypeTest.php
+++ tests/Unit/DataTypeTest.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\User;
 
-class UserTest extends TestCase
+class DataTypeTest extends TestCase
 {
     /**
      * A basic unit test example.
